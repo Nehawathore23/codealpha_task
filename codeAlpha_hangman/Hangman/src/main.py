f83df71999b443626c7c70f727b4d43cc8b28e91
nehawathore <your_github_email@example.com>
2025-12-09 11:59:31 +0530

    Add src folder as normal files after removing submodule

---
+++ codeAlpha_hangman/Hangman/src/main.py
@@ -0,0 +1,30 @@
+# main.py
+from game import Hangman
+
+def main():
+    print("===== HANGMAN GAME =====")
+
+    game = Hangman()
+
+    while True:
+        print("\nWord:", game.display_word())
+        print(f"Wrong Attempts: {game.wrong_attempts} / {game.max_attempts}")
+
+        guess = input("Enter a letter: ").lower()
+
+        if not guess.isalpha() or len(guess) != 1:
+            print("Please enter a single valid letter!")
+            continue
+
+        game.guess_letter(guess)
+
+        if game.is_won():
+            print("\n🎉 Congratulations! You guessed the word:", game.word)
+            break
+
+        if game.is_lost():
+            print("\n❌ You lost! The word was:", game.word)
+            break
+
+if __name__ == "__main__":
+    main()
